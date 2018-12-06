84998d36e5c197dce329fe019e81fc0ae8f1f5df
tristanmercille <tristanmercille@gmail.com>
2018-12-06 17:43:05 -0500

    first commit

---
+++ client.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+res.writeHead(200, {"Content-Type": "text/html"});
+res.end('<p>Here is a paragraph of <strong>HTML</strong>!</p>');
+});
+
+server.listen(8080);

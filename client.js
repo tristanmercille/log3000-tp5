ba04838b502cd32b2e40c222a89686c270f7f58d
tristanmercille <tristanmercille@gmail.com>
2018-12-06 18:01:22 -0500

    blue app

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-res.writeHead(200, {"Content-Type": "text/html"});
-res.end('<p>Here is a paragraph of <strong>HTML</strong>!</p>');
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end('<div style="background-color: blue; width:100%; height:100%"></div>');
 });
 
 server.listen(8080);

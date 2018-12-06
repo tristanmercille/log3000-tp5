0a770c8400b04180e5b201d8f8e4d197020a4134
tristanmercille <tristanmercille@gmail.com>
2018-12-06 18:20:49 -0500

    green

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.end('<div style="background-color: blue; width:100%; height:100%"></div>');
+    res.end('<div style="background-color: green; width:100%; height:100%"></div>');
 });
 
 server.listen(8080);
